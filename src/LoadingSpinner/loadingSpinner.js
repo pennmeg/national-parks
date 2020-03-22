ab3409f485ee810fb9fbb89b63bed4f889631914
Megan Penn <penn.meg@gmail.com>
2020-03-22 12:40:16 -0400

    added loading spinner for long calls

---
+++ src/LoadingSpinner/loadingSpinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './loadingSpinner.scss';
+
+const LoadingSpinner = () => {
+  return <div className="spinner" aria-label="Loading"></div>
+}
+
+export default LoadingSpinner;
