6d9436e3fb678adef3a961b0a1d49608ece37bb1
Megan Penn <penn.meg@gmail.com>
2020-03-22 14:45:48 -0400

    Rename api key to make it easier to change for other users

--- src/NationalParks/constants/constants.js
+++ src/NationalParks/constants/constants.js
@@ -1,3 +1,5 @@
+const myApiKey = process.env.REACT_APP_NATIONAL_PARK_API_KEY;
+
 export const npsUrl = `https://developer.nps.gov/api/v1/`;
 export const parkApi = `parks?`;
-export const apiKey = `&api_key=${process.env.REACT_APP_NATIONAL_PARK_API_KEY}`;
+export const apiKey = `&api_key=${myApiKey}`;
