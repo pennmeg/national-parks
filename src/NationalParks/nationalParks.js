e85c37e6e0325f024a1f1cc0857a88e7b1af2590
Megan Penn <penn.meg@gmail.com>
2020-03-22 11:37:48 -0400

    WIP

--- src/NationalParks/nationalParks.js
+++ src/NationalParks/nationalParks.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import usStates from './config/states.config.js';
+import './nationalParks.css';
+import parkSVG from '../assets/example-26.svg';
 
 function NationalPark() {
-  const [state, setState] = useState('AK');
+  const [state, setState] = useState('AL');
+  const [isLoading, setIsLoading] = useState(false);
+  const [stateParks, setStateParks] = useState([]);
+  const [error, setError] = useState(false);
 
   const handleChange = (value) => {
     if (value === state) {
@@ -13,29 +18,44 @@ function NationalPark() {
   };
 
   const searchState = () => {
-
-
+    console.log(state);
+    const npsUrl = `https://developer.nps.gov/api/v1/`;
+    const parkApi = `parks?`;
+    const stateCode = `stateCode=${state}`;
+    const apiKey = `&api_key=${process.env.REACT_APP_NATIONAL_PARK_API_KEY}`;
+    const url = npsUrl + parkApi + stateCode + apiKey;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setStateParks(data.data))
+      .catch(err => setError(true))
   }
+
   return (
-    <div id="nationalPark">
-      <header className="App-header">
-        <h1>National Parks</h1>
-        <p id="selectState">Select a state to begin</p>
-        <form aria-describedby="selectState">
-          <label for="state" className="srOnly">State</label>
-          <select id="state" value={state} onChange={e => handleChange(e.target.value)}>
-            {usStates.map(state => {
-              return <option value={state.state} key='usState-`${state.state}`'>{state.state}</option>;
-            })}
-          </select>
-          <button onClick={() => searchState()} type="submit">Search</button>
-        </form>
+    <>
+      <header className="nationalParks--header flexBox--justifyCenter">
+        <div>
+          <h1>U.S. National Parks</h1>
+          <p id="stateDesc">Select a state to begin searching for a park!</p>
+          <form aria-labelledby="stateDesc" className="stateForm flexBox">
+            <label for="stateSelect" className="srOnly">State</label>
+            <select id="stateSelect" value={state} onChange={e => handleChange(e.target.value)}>
+              {usStates.map(state => {
+                return <option value={state.state} key={`us-state-${state.state}`}>{state.state}</option>;
+              })}
+            </select>
+            <button onClick={() => searchState()} type="button">Search</button>
+          </form>
+        </div>
+        <img role="img" src={parkSVG} alt="" />
       </header>
       <div>
         <div>
           <div>
-            <h2>National Parks in STATE</h2>
-            <ul>
+            <h2 id="stateParks">National Parks {stateParks && stateParks.lentgh && `in ${state}`}</h2>
+            <ul aria-labelledby="stateParks">
+              {stateParks && stateParks.map(park => {
+                return <li role="none"><button type="button">{park.fullName}</button></li>
+              })}
             </ul>
           </div>
           <div>
@@ -45,7 +65,7 @@ function NationalPark() {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
